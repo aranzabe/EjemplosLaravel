d8c8f77c54302b895694484ff1344bc5660aaa66
Fernando <faranzabe@gmail.com>
2020-11-27 13:11:46 +0100

    Commit 2 Ejemplo BBDD

---
+++ ejemploBBDD/routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('indice', function () {
+    return view('welcome');
+});
+
+Route::get('listar','micontrol@listar');
+
+Route::get('pruebafaker','micontrol@pruebaFaker');
+
+Route::get('aptizaAlumnos','micontrol@actualizarAptiza');
+
+Route::get('aptizaProfes','micontrol@actualizarAptizaProfes');
+
+Route::get('aptizaProfesDelicados','micontrol@actualizarAptizaProfesDelicados');
+
+Route::get('cambiarEmailProfes','micontrol@cambiarEmail');
