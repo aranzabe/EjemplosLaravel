7f38ee77f12f390853a61644f2b17cceb5cb839a
Fernando <faranzabe@gmail.com>
2021-01-20 18:23:19 +0100

    Commit ejemplo API Rest Passport laravel8 Enero 2021

---
+++ ejemApiPassport/routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+//// Registration Routes...
+Route::get('registro', 'Auth\RegisterController@showRegistrationForm')->name('registro');
+Route::post('registro', 'Auth\RegisterController@register');
+
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
