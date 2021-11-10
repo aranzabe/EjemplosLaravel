ed6ab936f46188e9eb20005c807d59fbf1cd68e9
Fernando <faranzabe@gmail.com>
2021-11-10 08:43:34 +0100

    BD 10 nov

--- ejemploBBDD/resources/views/listado.blade.php
+++ ejemploBBDD/resources/views/listado.blade.php
@@ -3,36 +3,7 @@
     <head>
         <title>Laravel</title>
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
+        
     </head>
     <body>
         <?php
