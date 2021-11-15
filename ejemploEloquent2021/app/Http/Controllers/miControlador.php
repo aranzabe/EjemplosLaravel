9629ad391e04fdbaaba4cdc232460ce29db6979a
aranzabe <faranzabe@gmail.com>
2021-11-16 00:39:32 +0100

    Eloquent 2021 - 16 nov

---
+++ ejemploEloquent2021/app/Http/Controllers/miControlador.php
@@ -0,0 +1,240 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Persona; //--> Añadimos el espacio de direcciones de la clase Persona.
+use App\Models\Propiedad;
+use App\Models\Coche;
+
+class miControlador extends Controller
+{
+    public function verPersonas() {
+        $pers = Persona::all();
+        $datos = [
+            'pers' => $pers
+        ];
+
+        return view('vistapersonas', $datos);
+    }
+
+    //------------------------------------------------------------------------
+    public function probarFind() {
+        $pers = Persona::find('4D');
+        $datos = [
+            'pers' => $pers
+        ];
+        return view('verFind', $datos);
+    }
+
+    //------------------------------------------------------------------------
+    public function buscarPersona(Request $req) {
+        $dn = $req->get('DNIBuscado');
+        //Opción A.
+        //$pers = Persona::where('DNI', $dn)->first();
+        //Opción B.
+        $pers = Persona::find($dn);
+
+        $datos = [
+            'pers' => $pers
+        ];
+
+        return view('resultadoBusqueda', $datos);
+    }
+
+    //------------------------------------------------------------------------
+    public function insertarPersonas(Request $req) {
+        $dn = $req->get('DNI');
+        $no = $req->get('Nombre');
+        $tf = $req->get('Tfno');
+        $ed = $req->get('Edad');
+
+        $pe = new Persona;
+        $pe->DNI = $dn;
+        $pe->Nombre = $no;
+        $pe->Tfno = $tf;
+        $pe->edad = $ed;
+        $mensaje = 'Inserción ok';
+        try {
+            $pe->save();
+        } catch (\Exception $e) {
+            $mensaje = 'Clave duplicada';
+        }
+        return view('resultadoInsercion', ['mens' => $mensaje]);
+    }
+
+    //------------------------------------------------------------------------
+    public function vermayores() {
+        $pers = Persona::where('edad', '>', 18)
+                ->orderBy('Nombre', 'asc')
+                ->get();
+        $datos = [
+            'pers' => $pers
+        ];
+
+        return view('vistapersonasmayores', $datos);
+    }
+
+    //------------------------------------------------------------------------
+    public function verPersonasCoche() {
+//        $p = Propiedad::first();
+//        dd($p->DNI);
+
+
+        $p = Propiedad::all();
+        $vectorDatos = [];
+        foreach ($p as $pe) {
+            $pers = Persona::where('DNI', $pe->DNI)->first();
+            if ($pers) {
+                $vectorDatos[] = ['DNI' => $pe->DNI,
+                    'Matricula' => $pe->Matricula,
+                    'Nombre' => $pers->Nombre,
+                    'Tfno' => $pers->Tfno,
+                    'edad' => $pers->edad];
+            }
+        }
+        //dd($vectorDatos);
+        //dd($pers->Nombre);
+        //dd($p->propiedad);
+        //$p = Propiedad::first();
+        //dd($p->usuarios);
+        return view('verPersonasConCoche', ['pers' => $vectorDatos]);
+    }
+
+    //------------------------------------------------------------------------
+
+    /**
+     * Busca personas con coche usando la propiedad hasMany que tenemos definida en el modelo.
+     * @return type
+     */
+    public function probarMany() {
+//        //Opción A.
+//        $coches = Propiedad::with('usuarios')->get();
+////        dd($coches);  
+//        $conalquiler = [];
+//        foreach ($coches as $co) {
+//            foreach ($co->usuarios as $usu) {
+//                $datocoche = Coche::find($co->Matricula);
+//                $conalquiler[] = ['DNI' => $usu->DNI,
+//                    'Nombre' => $usu->Nombre,
+//                    'Matricula' => $co->Matricula,
+//                    'Marca' => $datocoche->Marca,
+//                    'Modelo' => $datocoche->Modelo];
+//            }
+//        }
+        //Opción B.
+//        $pers = Persona::with(['cochesAlquilados'])->get();
+//        $conalquiler = [];
+//        foreach ($pers as $pe) {
+//            //dd($pe->DNI);
+//            //dd($pe->cochesAlquilados);
+//            foreach ($pe->cochesAlquilados as $co) {
+////                dd($co);
+//                $conalquiler[] = ['DNI' => $pe->DNI,
+//                    'Nombre' => $pe->Nombre,
+//                    'Matricula' => $co->Matricula];
+//            }
+//        }
+//        dd($conalquiler);
+        //Opción C.
+        $coches = Propiedad::with(['usuarios', 'coches'])->get();  //Le pasamos las llamadas a los métodos de los modelos (usuarios y coches).
+//        dd($coches);  
+        $conalquiler = [];
+        foreach ($coches as $co) {
+//            dd($co->usuarios[0]->Nombre);
+            $conalquiler[] = [
+                'DNI' => $co->DNI,
+                'Nombre' => $co->usuarios[0]->Nombre,
+                'Matricula' => $co->Matricula,
+                'Marca' => $co->coches[0]->Marca,
+                'Modelo' => $co->coches[0]->Modelo
+            ];
+        }
+
+        
+        $datos = [
+            'pers' => $conalquiler
+        ];
+        return view('verMany', $datos);
+    }
+
+    //------------------------------------------------------------------------
+    public function probarManyUnaPersona(Request $req) {
+
+        $dni = $req->get('DNI');
+
+
+        //Opción A.
+//        $coches = Propiedad::with(['usuarios'])->where('DNI', $dni)->get();
+//        //dd($coches);
+//        $conalquiler = [];
+//        foreach ($coches as $co) {
+//            foreach ($co->usuarios as $usu) {
+//                $datocoche = Coche::find($co->Matricula);
+//                $conalquiler[] = ['DNI' => $usu->DNI,
+//                    'Nombre' => $usu->Nombre,
+//                    'Matricula' => $co->Matricula,
+//                    'Marca' => $datocoche->Marca,
+//                    'Modelo' => $datocoche->Modelo];
+//            }
+//        }
+        //Opción B.
+        $coches = Propiedad::with(['usuarios'])->where('DNI', $dni)->get();
+        
+        $conalquiler = [];
+        foreach ($coches as $co) {
+            //dd($co->usuarios[0]->Nombre);
+            //dd($co->usuarios[0]);
+            $conalquiler[] = [
+                'DNI' => $co->DNI,
+                'Nombre' => $co->usuarios[0]->Nombre,
+                'Matricula' => $co->Matricula,
+                'Marca' => $co->coches[0]->Marca,
+                'Modelo' => $co->coches[0]->Modelo
+            ];
+        }
+
+
+        $datos = [
+            'pers' => $conalquiler
+        ];
+        return view('verMany', $datos);
+    }
+
+
+    //-------------------------------------------------------------------
+    public function probarBelong() {
+        $coches = Propiedad::with(['usuariosBelong','cochesBelong'])->get();
+        dd($coches);
+    }
+
+    //-------------------------------------------------------------------
+    public function verComentarios(){
+        $comentarios = Persona::with(['comentarios'])->get();
+        //dd($comentarios);
+        $coments = [];
+        $i=0;
+        foreach ($comentarios as $co) {
+            //$valor = $co->DNI . ', '. $co->Nombre . ', '.$co->comentarios[0]->texto;
+            //dd($valor);
+            //dd($co->comentarios[0]->texto);
+            //dd($co->DNI);
+            //dd($co->Nombre);
+            $vecComentarios = [];
+            foreach($co->comentarios as $comentariosPersonales) {
+                //dd($comentariosPersonales);
+                $vecComentarios[] = $comentariosPersonales->texto;
+            }
+            $coments[] = [
+                'DNI' => $co->DNI,
+                'Nombre' => $co->Nombre,
+                'Comentario' => $vecComentarios
+            ];
+        }
+        //dd($coments);
+        $datos = [
+            'coms' => $coments
+        ];
+        return view('verComentariosClase',$datos);
+    }
+}
---
+++ ejemploEloquent2021/resources/views/resultadoBusqueda.blade.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+
+        <?php
+        if ($pers != null) {
+            echo 'Datos de la persona buscada.<br>';
+            echo 'DNI: ' . $pers->DNI . '<br>';
+            echo 'Nombre: ' . $pers->Nombre . '<br>';
+            echo 'Teléfono: ' . $pers->Tfno . '<br>';
+            echo 'Edad: ' . $pers->edad . '<br>';
+        } else {
+            echo 'Persona no encontrada.<br>';
+        }
+        ?>
+        <a href="volver">Volver</a>
+    </body>
+</html>
